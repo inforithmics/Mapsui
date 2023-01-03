cd8c3c9589b3d158508879a69ea90fbd11b06245
Thomas Stocker <thomas.stocker@gmail.com>
2023-01-03 03:28:14 +0100

    some skiasharp improvements

--- Mapsui.UI.Shared/GlobalUsingsUI.cs
+++ Mapsui.UI.Shared/GlobalUsingsUI.cs
@@ -6,24 +6,28 @@ global using BindableObject = Avalonia.AvaloniaObject;
 global using BindingMode = Avalonia.Data.BindingMode;
 #elif __WINUI__
 global using Microsoft.UI.Xaml;
+global using SkiaSharp.Views.Windows;
 
 global using BindableProperty = Microsoft.UI.Xaml.DependencyProperty;
 global using Point = Windows.Foundation.Point;
 global using BindingMode = Microsoft.UI.Xaml.Data.BindingMode;
 #elif __UWP__
 global using Windows.UI.Xaml;
+global using SkiaSharp.Views.UWP;
 
 global using BindableProperty = Windows.UI.Xaml.DependencyProperty;
 global using Point = Windows.Foundation.Point;
 global using BindingMode = Windows.UI.Xaml.Data.BindingMode;
 #elif __WPF__
 global using System.Windows;
+global using SkiaSharp.Views.WPF;
 
 global using BindableProperty = System.Windows.DependencyProperty;
 global using Point = System.Windows.Point;
 global using BindingMode = System.Windows.Data.BindingMode;
 #elif __MAUI__
 global using Microsoft.Maui.Controls;
+global using SkiaSharp.Views.Maui;
 
 global using Point = Microsoft.Maui.Graphics.Point;
 global using Color = Microsoft.Maui.Graphics.Color;
@@ -32,16 +36,19 @@ global using TextAlignment = Microsoft.Maui.TextAlignment;
 global using Thickness = Microsoft.Maui.Thickness;
 #elif __FORMS__
 global using Xamarin.Forms;
+global using SkiaSharp.Views.Forms;
 
 global using Color = Xamarin.Forms.Color;
 global using KnownColor = Xamarin.Forms.Color;
 global using Point = Xamarin.Forms.Point;
 #elif __ANDROID__
 global using Mapsui.UI.Utils;
+global using SkiaSharp.Views.Android;
 
 global using Point = Android.Graphics.Point;
 #elif __IOS__
 global using Mapsui.UI.Utils;
+global using SkiaSharp.Views.iOS;
 
 global using Point = System.Drawing.Point;
 #elif __ETO_FORMS__
